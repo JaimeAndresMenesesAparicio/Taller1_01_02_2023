663bf4ff5caf2d0955a2586cada090a072c295db
JaimeAndresMenesesAparicio <jaime.meneses89@gmail.com>
2023-02-01 23:08:06 -0500

    Ejercicio_5

---
+++ pedir_Datos_Por_Prompt_01_02_2023/main.js
@@ -0,0 +1,9 @@
+// 5.	Haz un programa de nombre main.js y otro llamado index.html
+//  incluir el archivo JavaScript en el <head></ head>, que has 
+//  de grabar en una carpeta llamada pedir_Datos_Por_Prompt_FechaActual, 
+//  En la ventana saldrá un texto que diga “Ingresar un numero” capturar 
+//  el valor en un variable y mostrar el valor por medio de la consola 
+//  o de la ventana emergente.
+
+let numero = prompt("Ingresar un número");
+alert(numero);
